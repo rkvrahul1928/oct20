666d8f0ac52dcba62f676da7e72cbe5c84ecbf7e
avana <avana@LAPTOP-NFK51C78>
2021-10-20 08:35:51 +0530

    Draft

---
+++ TestingFramework/src/test/java/generic/FileUtils.java
@@ -0,0 +1,44 @@
+package generic;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.Reporter;
+
+public class FileUtils {
+
+	public static String getPptValue(String file,String key)
+	{
+		String value="";
+		
+		Properties p =new Properties();
+		try 
+		{
+			p.load(new FileInputStream(file));
+			value = p.getProperty(key);
+		}
+		catch (Exception e)
+		{
+
+		}
+		
+		return value;
+	}
+	
+	public static String getXlCellValue(String path,String sheet,int r,int c)
+	{
+		String value="";
+		try {
+		Workbook wb = WorkbookFactory.create(new FileInputStream(path));
+		value = wb.getSheet(sheet).getRow(r).getCell(c).getStringCellValue();
+		wb.close();
+		}
+		catch (Exception e) {
+		}
+		return value;
+	}
+}
